212d043e917e8278d0618e7814eec2f821a735a7
Sergio Cuadrado <sergiocuadrado@gmail.com>
2024-03-06 16:24:06 +0100

    Add ESLint configurations for Airbnb base and Next.js

--- index.js
+++ index.js
@@ -5,60 +5,34 @@ module.exports = {
     'airbnb-typescript',
     'airbnb/hooks',
     'plugin:prettier/recommended',
+    "airbnb-typescript/base",
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
     project: './tsconfig.json',
   },
-  plugins: ['simple-import-sort'],
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "prettier"
+  ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/explicit-member-accessibility': [
-      'error',
-      { overrides: { constructors: 'no-public' } },
-    ],
-    '@typescript-eslint/lines-between-class-members': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    'class-methods-use-this': 'off',
-    curly: ['error', 'all'],
-    'func-names': ['warn', 'as-needed'],
-    'import/no-default-export': 'warn',
-    'import/prefer-default-export': 'off',
-    'max-classes-per-file': 'off',
-    'no-await-in-loop': 'off',
-    'no-continue': 'off',
-    'no-plusplus': 'off',
-    'no-restricted-globals': 'off',
-    'no-restricted-syntax': [
-      'error',
+    "quotes": [
+      2,
+      "double",
       {
-        selector: 'ForInStatement',
-        message:
-          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
-      },
-      {
-        selector: 'LabeledStatement',
-        message:
-          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
-      },
-      {
-        selector: 'WithStatement',
-        message:
-          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
-      },
+        "avoidEscape": true
+      }
     ],
-    'no-use-before-define': 'off',
-    'prettier/prettier': [
-      'error',
-      { arrowParens: 'avoid', printWidth: 100, singleQuote: true, trailingComma: 'all' },
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
     ],
-    radix: 'off',
-    'react/function-component-definition': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/prop-types': 'off',
-    'react/require-default-props': 'off',
-    'simple-import-sort/imports': 'warn',
+    "@typescript-eslint/no-shadow": "off",
+    "@typescript-eslint/naming-convention": "off"
   },
 };
