ce474014140263b000306e4a54bd04fe4a813748
Sergio Cuadrado <sergiocuadrado@gmail.com>
2024-03-11 13:30:20 +0100

    Refactor code for improved performance

--- index.js
+++ index.js
@@ -37,9 +37,6 @@ module.exports = {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
-    // react: {
-    //   version: react ? "detect" : "17.0.0",
-    // },
   },
   rules: {
     "no-console": "warn",
@@ -60,7 +57,19 @@ module.exports = {
     "no-restricted-globals": ["error", "isFinite", "isNaN"].concat(
       confusingBrowserGlobals
     ),
-
+    // TypeScript's `!` non-null assertion operator asserts to the type system that an expression is non-nullable, as in not null or undefined
+    "@typescript-eslint/no-non-null-assertion": "error",
+    'class-methods-use-this': 'off',
+    // Funciones con nombre
+    'func-names': ['warn', 'as-needed'],
+    // Solo se puede tener una clase por archivo
+    'max-classes-per-file': ['error', 1],
+    // No se pueden usar funciones asíncronas dentro de un bucle
+    'no-await-in-loop': 'error',
+    // Poder utilizar el `continue` en los bucles
+    'no-continue': 'off',
+    // Poder hacer `++` y `--`
+    'no-plusplus': 'off',
     // too many false positives with aliases/root dirs
     "import/no-unresolved": "off",
 
@@ -72,241 +81,96 @@ module.exports = {
     "@typescript-eslint/no-shadow": "off",
     "@typescript-eslint/naming-convention": "off",
 
-      // Regla para el orden de los miembros en las clases
-      // Atributos → Constructor → Public → Protected → Private
-      "@typescript-eslint/member-ordering": [
-        "error",
-        {
-          default: [
-            // Atributos
-            "public-static-field",
-            "protected-static-field",
-            "private-static-field",
-            "public-instance-field",
-            "protected-instance-field",
-            "private-instance-field",
-            // Constructor
-            "constructor",
-            // Métodos públicos
-            "public-instance-method",
-            // Métodos protegidos
-            "protected-instance-method",
-            // Métodos privados
-            "private-instance-method"
-          ]
-        }
-      ],
+    // Regla para el orden de los miembros en las clases
+    // Atributos → Constructor → Public → Protected → Private
+    "@typescript-eslint/member-ordering": [
+      "error",
+      {
+        default: [
+          // Atributos
+          "public-static-field",
+          "protected-static-field",
+          "private-static-field",
+          "public-instance-field",
+          "protected-instance-field",
+          "private-instance-field",
+          // Constructor
+          "constructor",
+          // Métodos públicos
+          "public-instance-method",
+          // Métodos protegidos
+          "protected-instance-method",
+          // Métodos privados
+          "private-instance-method"
+        ]
+      }
+    ],
 
 
-      "no-unused-vars": [
-        "error",
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          varsIgnorePattern: "^_",
-          ignoreRestSiblings: true,
-        },
-        // {
-        //   "varsIgnorePattern": "^is[A-Z].*"
-        // }
-      ],
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
 
-      '@typescript-eslint/naming-convention': [
-        'error',
-        {
-          // Los nombres de las clases deben ser en PascalCase
-          selector: 'class',
-          format: ['PascalCase'],
-          leadingUnderscore: 'forbid',
-          trailingUnderscore: 'forbid',
-        },
-        // las variables en camelCase
-        {
-          selector: 'variable',
-          format: ['camelCase'],
-          leadingUnderscore: 'forbid',
-          trailingUnderscore: 'forbid',
-        },
-        // las variables privadas en camelCase con un guión bajo al principio
-        {
-          "selector": "memberLike",
-          "format": ["camelCase"],
-          "leadingUnderscore": "forbid"
-        },
-        {
-          "selector": "memberLike",
-          "modifiers": ["private"],
-          "format": ["camelCase"],
-          "leadingUnderscore": "require"
-        }
-      ],
-        "@typescript-eslint/consistent-type-imports": [
-          "error",
-          { fixStyle: "inline-type-imports" },
-        ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        // Los nombres de las clases deben ser en PascalCase
+        selector: 'class',
+        format: ['PascalCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      // las variables en camelCase
+      {
+        selector: 'variable',
+        format: ['camelCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      // las variables privadas en camelCase con un guión bajo al principio
+      {
+        "selector": "memberLike",
+        "format": ["camelCase"],
+        "leadingUnderscore": "forbid"
+      },
+      {
+        "selector": "memberLike",
+        "modifiers": ["private"],
+        "format": ["camelCase"],
+        "leadingUnderscore": "require"
+      }
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { fixStyle: "inline-type-imports" },
+    ],
 
-        // I want correct tsdoc syntax
-        "tsdoc/syntax": "warn",
+    // I want correct tsdoc syntax
+    "tsdoc/syntax": "warn",
 
-        "react/prop-types": "off",
+    "react/prop-types": "off",
 
-        // have to disable since it can report incorrect errors
-        "no-empty-function": "off",
+    // have to disable since it can report incorrect errors
+    "no-empty-function": "off",
 
-        // I prefer shorthand syntax
-        "@typescript-eslint/array-type": ["error", { default: "array" }],
+    // I prefer shorthand syntax
+    "@typescript-eslint/array-type": ["error", { default: "array" }],
 
-        // I prefer using `interface` over `type = {}`
-        "@typescript-eslint/consistent-type-definitions": [
-          "error",
-          "interface",
-        ],
-        // not a big fan of requiring unknown objects to require the index signature
-        "@typescript-eslint/ban-types": "off",
+    // not a big fan of requiring unknown objects to require the index signature
+    "@typescript-eslint/ban-types": "off",
 
-        // This is a "better" version of the `noUnusedLocals` and
-        // `noUnusedParameters` from the tsconfig.json since it can catch
-        // unused vars in rest parameters that weren't meant to be omitted. I
-        // must manually rename to be _name so I know it was intentionally
-        // omitted
-        // "@typescript-eslint/no-unused-vars": [
-        //   "error",
-        //   {
-        //     argsIgnorePattern: "^_",
-        //     varsIgnorePattern: "^_",
-        //   },
-        // ],
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "off",
 
-        "no-use-before-define": "off",
-        "@typescript-eslint/no-use-before-define": [
-          "warn",
-          { ignoreTypeReferences: true },
-        ],
+    // Easy autofixable import sorting.
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error"
   },
-  // files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-  overrides: [
-    // {
-    //   files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-    //   rules: {
-    //     // Regla para el orden de los miembros en las clases
-    //     // Atributos → Constructor → Public → Protected → Private
-    //     "@typescript-eslint/member-ordering": [
-    //       "error",
-    //       {
-    //         default: [
-    //           // Atributos
-    //           "public-static-field",
-    //           "protected-static-field",
-    //           "private-static-field",
-    //           "public-instance-field",
-    //           "protected-instance-field",
-    //           "private-instance-field",
-    //           // Constructor
-    //           "constructor",
-    //           // Métodos públicos
-    //           "public-instance-method",
-    //           // Métodos protegidos
-    //           "protected-instance-method",
-    //           // Métodos privados
-    //           "private-instance-method"
-    //         ]
-    //       }
-    //     ],
-
-
-    //     "no-unused-vars": [
-    //       "error",
-    //       {
-    //         vars: "all",
-    //         varsIgnorePattern: "^_",
-    //         args: "after-used",
-    //         varsIgnorePattern: "^_",
-    //         ignoreRestSiblings: true,
-    //       },
-    //       // {
-    //       //   "varsIgnorePattern": "^is[A-Z].*"
-    //       // }
-    //     ],
-
-    //     '@typescript-eslint/naming-convention': [
-    //       'error',
-    //       {
-    //         // Los nombres de las clases deben ser en PascalCase
-    //         selector: 'class',
-    //         format: ['PascalCase'],
-    //         leadingUnderscore: 'forbid',
-    //         trailingUnderscore: 'forbid',
-    //       },
-    //       // las variables en camelCase
-    //       {
-    //         selector: 'variable',
-    //         format: ['camelCase'],
-    //         leadingUnderscore: 'forbid',
-    //         trailingUnderscore: 'forbid',
-    //       },
-    //       // las variables privadas en camelCase con un guión bajo al principio
-    //       {
-    //         "selector": "memberLike",
-    //         "format": ["camelCase"],
-    //         "leadingUnderscore": "forbid"
-    //       },
-    //       {
-    //         "selector": "memberLike",
-    //         "modifiers": ["private"],
-    //         "format": ["camelCase"],
-    //         "leadingUnderscore": "require"
-    //       }
-    //     ],
-    //   },
-    // },
-    // {
-    //   files: ["**/*.ts", "**/*.tsx"],
-    //   extends: ["plugin:@typescript-eslint/recommended"],
-    //   rules: {
-    //     "@typescript-eslint/consistent-type-imports": [
-    //       "error",
-    //       { fixStyle: "inline-type-imports" },
-    //     ],
-
-    //     // I want correct tsdoc syntax
-    //     "tsdoc/syntax": "warn",
-
-    //     "react/prop-types": "off",
-
-    //     // have to disable since it can report incorrect errors
-    //     "no-empty-function": "off",
-
-    //     // I prefer shorthand syntax
-    //     "@typescript-eslint/array-type": ["error", { default: "array" }],
-
-    //     // I prefer using `interface` over `type = {}`
-    //     "@typescript-eslint/consistent-type-definitions": [
-    //       "error",
-    //       "interface",
-    //     ],
-    //     // not a big fan of requiring unknown objects to require the index signature
-    //     "@typescript-eslint/ban-types": "off",
-
-    //     // This is a "better" version of the `noUnusedLocals` and
-    //     // `noUnusedParameters` from the tsconfig.json since it can catch
-    //     // unused vars in rest parameters that weren't meant to be omitted. I
-    //     // must manually rename to be _name so I know it was intentionally
-    //     // omitted
-    //     // "@typescript-eslint/no-unused-vars": [
-    //     //   "error",
-    //     //   {
-    //     //     argsIgnorePattern: "^_",
-    //     //     varsIgnorePattern: "^_",
-    //     //   },
-    //     // ],
-
-    //     "no-use-before-define": "off",
-    //     "@typescript-eslint/no-use-before-define": [
-    //       "warn",
-    //       { ignoreTypeReferences: true },
-    //     ],
-    //   },
-    // },
-  ],
 };
